19938ae69f73a66bed64fd30e6ce920fecf320e2
BenGee1996 <bengraham20@gmail.com>
2022-06-22 18:04:39 +0100

    A sorted medals table

--- powershell/medals.ps1
+++ powershell/medals.ps1
@@ -6,7 +6,48 @@ function Build-MedalTable {
         $Medals
     )
 
-    # Add your code here
-    # Create a medal table based on the provided result list
     # First place gives 3 points, second place 2 points and third place 1 point
-}
+
+    $Scoring = [PSCustomObject]@{
+        1 = 3
+        2 = 2
+        3 = 1
+    }
+
+    $MedalTable = @{}
+
+    foreach ($Result in $Medals.podium) {
+
+        $Position = $Result.split(".")[0]
+        $Country = $Result.Split(".")[1]
+
+        if ($MedalTable.Keys -contains $Country) {
+            $MedalTable.$Country += $Scoring.$Position
+        } else {
+            $MedalTable.$Country = $Scoring.$Position
+        }    
+    }
+
+   return $MedalTable.GetEnumerator() | sort Value -descending
+}   
+
+    $MedalResults = @(
+        @{
+            'sport' = 'cycling'
+            'podium' = @('1.China', '2.Germany', '3.ROC')
+        }
+        @{
+            'sport' = 'fencing'
+            'podium' = @('1.ROC', '2.France', '3.Italy')
+        }
+        @{
+            'sport' = 'high jump'
+            'podium' = @('1.Italy', '1.Qatar', '3.Belarus')
+        }
+        @{
+            'sport' = 'swimming'
+            'podium' = @('1.USA', '2.France', '3.Brazil')
+        }
+    )
+
+Build-MedalTable -Medals $MedalResults
